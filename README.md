d9f0274abcc3b3d40535b29a5dc358c6005d2b82
Emanuel Rabina <emanuelrabina@gmail.com>
2024-07-07 10:29:16 +1200

    Allow advance() for all buffer types

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Add a dependency to your project with the following co-ordinates:
  - ArtifactId: `groovy-extensions`
  - Version: `2.3.1`
 
-Check the [project releases](https://github.com/ultraq/groovy-extensions/releases)
+Check the [project tags](https://github.com/ultraq/groovy-extensions/tags)
 for a list of available versions.
 
 
--- source/nz/net/ultraq/groovy/extensions/ByteBufferExtensions.groovy
+++ source/nz/net/ultraq/groovy/extensions/ByteBufferExtensions.groovy
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright 2019, Emanuel Rabina (http://www.ultraq.net.nz/)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,29 +22,16 @@ import java.nio.ByteBuffer
 
 /**
  * Instance method extensions to the {@code ByteBuffer} class.
- * 
+ *
  * @author Emanuel Rabina
  */
 class ByteBufferExtensions {
 
 	/**
-	 * Advance the internal position of a buffer by the given amount.  Negative
-	 * values may be given to move the position backwards.
-	 * 
-	 * @param self
-	 * @param n
-	 * @return The buffer.
-	 */
-	static ByteBuffer advance(ByteBuffer self, int n) {
-
-		return self.position(self.position() + n)
-	}
-
-	/**
 	 * An alias for {@link ByteBuffer#get} so that its shortened Groovy getter
 	 * form looks consistent alongside the other shortened getters like short,
 	 * int, etc.
-	 * 
+	 *
 	 * @param self
 	 * @return
 	 */
@@ -79,7 +66,7 @@ class ByteBufferExtensions {
 	 * A relative bulk <i>put</i> method using another {@code ByteBuffer} but only
 	 * reading up to {@code length} bytes from that buffer.  The position of both
 	 * buffers will be increased by {@code length}.
-	 * 
+	 *
 	 * @param self
 	 * @param src
 	 * @param length
@@ -96,7 +83,7 @@ class ByteBufferExtensions {
 	 * Split a buffer into several smaller buffers of the specified size.  If the
 	 * size doesn't cleanly divide into the current buffer, then the final buffer
 	 * will be smaller and contain the remaining bytes.
-	 * 
+	 *
 	 * @param self
 	 * @param length
 	 * @return
