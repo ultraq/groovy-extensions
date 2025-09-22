fa8a96db9453ba3f1e8550d93c99b153bca0af9e
Emanuel Rabina <emanuelrabina@gmail.com>
2025-09-22 19:57:08 +1200

    Expand drain() to accept a target list

--- source/nz/net/ultraq/groovy/extensions/BlockingQueueExtensions.groovy
+++ source/nz/net/ultraq/groovy/extensions/BlockingQueueExtensions.groovy
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright 2019, Emanuel Rabina (http://www.ultraq.net.nz/)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,36 +20,28 @@ import java.util.concurrent.BlockingQueue
 
 /**
  * Extensions to the {@link BlockingQueue} interface.
- * 
+ *
  * @author Emanuel Rabina
  */
 class BlockingQueueExtensions {
 
 	/**
-	 * Drains all available elements to a new list which is returned.
-	 * 
-	 * @param self
-	 * @return
+	 * Drains all available elements, either to a new list or a supplied one,
+	 * which is also returned.
 	 */
-	static <E> List<E> drain(BlockingQueue<E> self) {
+	static <E> List<E> drain(BlockingQueue<E> self, List<E> target = []) {
 
-		def remaining = []
-		self.drainTo(remaining)
-		return remaining
+		self.drainTo(target)
+		return target
 	}
 
 	/**
-	 * Drain at most the given number of elements into a new list which is
-	 * returned.
-	 * 
-	 * @param self
-	 * @param maxElements
-	 * @return
+	 * Drain at most the given number of elements, either to a new list or a
+	 * supplied one, which is also returned.
 	 */
-	static <E> List<E> drain(BlockingQueue<E> self, int maxElements) {
+	static <E> List<E> drain(BlockingQueue<E> self, List<E> target = [], int maxElements) {
 
-		def elements = []
-		self.drainTo(elements, maxElements)
-		return elements
+		self.drainTo(target, maxElements)
+		return target
 	}
 }
--- test/nz/net/ultraq/groovy/extensions/BlockingQueueExtensionsTests.groovy
+++ test/nz/net/ultraq/groovy/extensions/BlockingQueueExtensionsTests.groovy
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright 2022, Emanuel Rabina (http://www.ultraq.net.nz/)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,33 +22,36 @@ import java.util.concurrent.ArrayBlockingQueue
 
 /**
  * Tests for the {@link BlockingQueueExtensions} methods.
- * 
+ *
  * @author Emanuel Rabina
  */
 class BlockingQueueExtensionsTests extends Specification {
 
 	def data = [1, 2, 3, 4]
 
-	def "drain() is a shortcut for BlockingQueue.drainTo"() {
-		given:
-			def queue = new ArrayBlockingQueue(4, true, data)
-		when:
-			def result = queue.drain()
-		then:
-			def expected = []
-			new ArrayBlockingQueue(4, true, data).drainTo(expected)
-			result == expected
+	def "drain() receives all queued elements"() {
+		expect:
+			new ArrayBlockingQueue(4, true, data).drain() == data
 	}
 
-	def "drain(int) is a shortcut for BlockingQueue.drainTo(int)"() {
-		given:
-			def queue = new ArrayBlockingQueue(4, true, data)
-			def maxElements = 3
-		when:
-			def result = queue.drain(maxElements)
-		then:
-			def expected = []
-			new ArrayBlockingQueue(4, true, data).drainTo(expected, maxElements)
-			result == expected
+	def "drain() receives all queued elements - provided target list"() {
+		expect:
+			var target = []
+			var result = new ArrayBlockingQueue(4, true, data).drain(target)
+			result == data
+			result === target
+	}
+
+	def "drain(int) receives the given number of elements"() {
+		expect:
+			new ArrayBlockingQueue(4, true, data).drain(3) == data.take(3)
+	}
+
+	def "drain(int) receives the given number of elements - provided target list"() {
+		expect:
+			var target = []
+			var result = new ArrayBlockingQueue(4, true, data).drain(target, 3)
+			result == data.take(3)
+			result === target
 	}
 }
