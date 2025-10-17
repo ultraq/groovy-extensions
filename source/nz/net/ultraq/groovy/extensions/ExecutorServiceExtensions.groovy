230c1e5b951557257897206234e9051ff7cf5fc3
Emanuel Rabina <emanuelrabina@gmail.com>
2025-10-17 18:48:24 +1300

    Add note about Java 19+ closeable and log unresponsive task count

--- source/nz/net/ultraq/groovy/extensions/ExecutorServiceExtensions.groovy
+++ source/nz/net/ultraq/groovy/extensions/ExecutorServiceExtensions.groovy
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright 2019, Emanuel Rabina (http://www.ultraq.net.nz/)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,9 @@
 
 package nz.net.ultraq.groovy.extensions
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 import groovy.transform.stc.ClosureParams
 import groovy.transform.stc.SimpleType
 import java.util.concurrent.ExecutorService
@@ -23,22 +26,29 @@ import java.util.concurrent.TimeUnit
 
 /**
  * Extensions to the {@code ExecutorService} interface.
- * 
+ *
  * @author Emanuel Rabina
  */
 class ExecutorServiceExtensions {
+
+	private static Logger logger = LoggerFactory.getLogger(ExecutorServiceExtensions)
 
 	/**
 	 * Execute the given closure, performing a shutdown after it has exited (see
 	 * {@link #shutdownAwaitTermination(ExecutorService, int, TimeUnit)}).
 	 * <pre>
-	 *   def executorService = Executors.newCachedThreadPool()
+	 *   var executorService = Executors.newCachedThreadPool()
 	 *   executorService.executeAndShutdown { e ->
 	 *     // Use the executorService within this closure
 	 *   }
 	 *   assert executorService.shutdown
 	 * </pre>
-	 * 
+	 * <p>Note that Java 19 made {@link ExecutorService} implement {@link AutoCloseable}
+	 * and the {@code close} method basically does what this method does, but with
+	 * a default await value of 1 day (!!!).  Still, if tasks are taking any
+	 * period of time after shutdown then there might be something worth
+	 * investigating.
+	 *
 	 * @param <T>
 	 * @param self
 	 * @param awaitValue
@@ -62,8 +72,9 @@ class ExecutorServiceExtensions {
 
 	/**
 	 * Initiate a shutdown, waiting the specified amount of time before forcing
-	 * termination.
-	 * 
+	 * termination.  A default value of 5 seconds is used if neither await value
+	 * is specified.
+	 *
 	 * @param self
 	 * @param awaitValue
 	 * @param awaitUnit
@@ -71,8 +82,17 @@ class ExecutorServiceExtensions {
 	static void shutdownAwaitTermination(ExecutorService self, int awaitValue = 5, TimeUnit awaitUnit = TimeUnit.SECONDS) {
 
 		self.shutdown()
-		if (!self.awaitTermination(awaitValue, awaitUnit)) {
+		try {
+			if (!self.awaitTermination(awaitValue, awaitUnit)) {
+				var tasks = self.shutdownNow()
+				if (tasks) {
+					logger.warn('ExecutorService was foricbly terminated, but {} tasks did not respond', tasks.size())
+				}
+			}
+		}
+		catch (InterruptedException ignored) {
 			self.shutdownNow()
+			Thread.currentThread().interrupt()
 		}
 	}
 }
